74e6eaa5a755b93f5ae2b6ac12ab3682d9c78a30
Brandon Stamba <brandonstamba@gmail.com>
2023-07-13 14:24:46 -0500

    Version 3 GitTutorial

--- src/gitTutorialPackage/gitTutorialMain.java
+++ src/gitTutorialPackage/gitTutorialMain.java
@@ -4,6 +4,6 @@ public class gitTutorialMain {
 	public static void main(String[] args) {
 		gitTutorial g = new gitTutorial();
 		g.getMessage();
-		System.out.println("This is Version 2.");
+		System.out.println("This is Version 3.");
 	}
 }
