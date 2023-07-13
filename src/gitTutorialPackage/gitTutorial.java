81386860f04df934e633dec2d5c5183870e626a4
Brandon Stamba <brandonstamba@gmail.com>
2023-07-13 14:23:42 -0500

    Version 1 GitTutorial

---
+++ src/gitTutorialPackage/gitTutorial.java
@@ -0,0 +1,8 @@
+package gitTutorialPackage;
+
+public class gitTutorial {
+
+	public void getMessage() {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/gitTutorialPackage/gitTutorialMain.java
@@ -0,0 +1,9 @@
+package gitTutorialPackage;
+public class gitTutorialMain {
+
+	public static void main(String[] args) {
+		gitTutorial g = new gitTutorial();
+		g.getMessage();
+		System.out.println("This is Version 1.");
+	}
+}
